f6416f1a09bbcbb76cdd8498be35040dc33cb108
Alexander Gonzalez <agonzalez@talana.com>
2022-06-22 12:48:53 -0400

    DB url

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:19495523Abc*@localhost:5433/TodoAppDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:19495523Abc*@localhost:5433/TodoAppDatabase"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
